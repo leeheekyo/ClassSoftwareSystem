2bf78c5bec9c6f7e2c289890a2ba0660c361ce20
cse <leeheekyo@github.com>
2017-03-07 09:48:36 +0900

    command edit

--- ClassSoftwareSystem/src/Main.java
+++ ClassSoftwareSystem/src/Main.java
@@ -3,7 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//print Hello World
+		System.out.println("Hello World!");
+		
+		
 	}
 
 }
